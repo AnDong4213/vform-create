4f84bcbe946a44d908aa8b529a12e3c4311ac54d
赵 安东 <zhaoandong@bobfintech.com.cn>
2022-08-12 15:37:19 +0800

    feat: 测试本地包

---
+++ src/components/form-render/container-item/container-item-wrapper.vue
@@ -0,0 +1,28 @@
+<!--
+* author: byjk
+-->
+
+<template>
+  <div class="container-wrapper" :class="[customClass]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "container-item-wrapper",
+    props: {
+      widget: Object,
+    },
+    computed: {
+      customClass() {
+        return !!this.widget.options.customClass ? this.widget.options.customClass.join(' ') : ''
+      },
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/utils/config.js
@@ -0,0 +1,7 @@
+export const VARIANT_FORM_VERSION = '0.0.1'
+
+export const MOCK_CASE_URL = 'https://ks3-cn-beijing.ksyuncs.com/vform-static/vcase/'
+
+export const ACE_BASE_PATH = 'https://ks3-cn-beijing.ksyun.com/vform2021/ace-mini'
+
+export const BEAUTIFIER_PATH= 'https://ks3-cn-beijing.ksyun.com/vform2021/js-beautify/1.14.0/beautifier.min.js'
